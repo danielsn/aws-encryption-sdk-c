2bf9878539edb7e3152db81f5378ce865635e253
Daniel Schwartz-Narbonne <dsn@amazon.com>
2019-09-16 15:01:27 -0400

    aws_cryptosdk_keyring_release proof

---
+++ .cbmc-batch/jobs/aws_cryptosdk_keyring_release/aws_cryptosdk_keyring_release_harness.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+ * use
+ * this file except in compliance with the License. A copy of the License is
+ * located at
+ *
+ *     http://aws.amazon.com/apache2.0/
+ *
+ * or in the "license" file accompanying this file. This file is distributed on
+ * an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or
+ * implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <aws/cryptosdk/materials.h>
+#include <proof_helpers/cryptosdk/make_common_data_structures.h>
+#include <proof_helpers/make_common_data_structures.h>
+#include <proof_helpers/proof_allocators.h>
+#include <proof_helpers/utils.h>
+
+void destroy(struct aws_cryptosdk_keyring *ring) {
+    assert(ring);
+    assert(AWS_OBJECT_PTR_IS_WRITABLE(ring));
+    free(ring);
+}
+
+void aws_cryptosdk_keyring_release_harness() {
+    const struct aws_cryptosdk_keyring_vt vtable = { .vt_size    = sizeof(struct aws_cryptosdk_keyring_vt),
+                                                     .name       = ensure_c_str_is_allocated(SIZE_MAX),
+                                                     .destroy    = nondet_bool() ? destroy : NULL,
+                                                     .on_encrypt = nondet_voidp(),
+                                                     .on_decrypt = nondet_voidp() };
+    __CPROVER_assume(aws_cryptosdk_keyring_vt_is_valid(&vtable));
+
+    struct aws_cryptosdk_keyring *keyring = can_fail_malloc(sizeof(struct aws_cryptosdk_keyring));
+
+    if (keyring) {
+        keyring->vtable = &vtable;
+        __CPROVER_assume(aws_cryptosdk_keyring_base_is_valid(keyring));
+    }
+    aws_cryptosdk_keyring_release(keyring);
+}
--- include/aws/cryptosdk/materials.h
+++ include/aws/cryptosdk/materials.h
@@ -524,10 +524,10 @@ AWS_CRYPTOSDK_STATIC_INLINE bool aws_cryptosdk_keyring_vt_is_valid(const struct 
  * Constant time check of data-structure invariants for struct aws_cryptosdk_cmm. Since implementations of the
  * cmm may add additional fields, they may define their own, specialized is_valid functions that use this as a base.
  */
-AWS_CRYPTOSDK_STATIC_INLINE bool aws_cryptosdk_keyring_base_is_valid(const struct aws_cryptosdk_keyring *cmm) {
-    return AWS_OBJECT_PTR_IS_WRITABLE(cmm) && aws_atomic_var_is_valid(&cmm->refcount) &&
-           AWS_ATOMIC_VAR_INTVAL(&cmm->refcount) > 0 && AWS_ATOMIC_VAR_INTVAL(&cmm->refcount) <= SIZE_MAX &&
-           aws_cryptosdk_cmm_vtable_is_valid(cmm->vtable);
+AWS_CRYPTOSDK_STATIC_INLINE bool aws_cryptosdk_keyring_base_is_valid(const struct aws_cryptosdk_keyring *keyring) {
+    return AWS_OBJECT_PTR_IS_WRITABLE(keyring) && aws_atomic_var_is_valid(&keyring->refcount) &&
+           AWS_ATOMIC_VAR_INTVAL(&keyring->refcount) > 0 && AWS_ATOMIC_VAR_INTVAL(&keyring->refcount) <= SIZE_MAX &&
+           aws_cryptosdk_keyring_vt_is_valid(keyring->vtable);
 }
 
 /**
@@ -548,6 +548,7 @@ AWS_CRYPTOSDK_STATIC_INLINE void aws_cryptosdk_keyring_base_init(
  * Decrements the reference count on the keyring; if the new reference count is zero, the keyring is destroyed.
  */
 AWS_CRYPTOSDK_STATIC_INLINE void aws_cryptosdk_keyring_release(struct aws_cryptosdk_keyring *keyring) {
+    AWS_PRECONDITION(!keyring || aws_cryptosdk_keyring_base_is_valid(keyring));
     if (keyring && aws_cryptosdk_private_refcount_down(&keyring->refcount)) {
         AWS_CRYPTOSDK_PRIVATE_VF_CALL_NO_RETURN(destroy, keyring);
     }
